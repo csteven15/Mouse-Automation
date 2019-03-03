07c2a6ddea0eae8b60943d661344095565a00489
Steven Chen <chensteven88999@gmail.com>
2019-03-03 01:29:14 -0500

    updated times

--- mouseAutomation.go
+++ mouseAutomation.go
@@ -10,23 +10,30 @@ import (
 func main() {
 	fmt.Println("Starting script")
 
-	fmt.Println("Enter how many programs there are:")
-	var numApps int
-	fmt.Scanf("%d", &numApps)
-	fmt.Println("Scanning for", numApps, "clicks")
+	nextTime := time.Now().Truncate(time.Minute)
 
-	// apps : slice of slices
-	apps := make([][]int, numApps)
+	fmt.Println("Enter how many clicks there are:")
+	var numClicks int
+	fmt.Scanf("%d", &numClicks)
+	fmt.Println("Scanning for", numClicks, "clicks")
 
-	for i := range apps {
-		apps[i] = getAppLocation()
+	// clicks : slice of slices
+	clicks := make([][]int, numClicks)
+
+	for i := range clicks {
+		clicks[i] = getAppLocation()
 	}
 
-	for i := range apps {
-		fmt.Println("moving to ", apps[i])
-		robotgo.MoveMouse(apps[i][0], apps[i][1])
-		robotgo.MouseClick("left", true)
-		time.Sleep(4 * time.Second)
+	for {
+		for i := range clicks {
+			fmt.Println("Moving to", clicks[i])
+			// robotgo.MoveMouseSmooth(clicks[i][0], clicks[i][1], 1.0, 1.0)
+			robotgo.MoveMouse(clicks[i][0], clicks[i][1])
+			fmt.Println("Clicking on", clicks[i])
+			robotgo.MouseClick("left", true)
+			time.Sleep(10 * time.Second)
+		}
+		nextTime = nextTime.Add(time.Second * 10)
 	}
 
 }
@@ -35,7 +42,7 @@ func getAppLocation() []int {
 	getPoints := robotgo.AddEvent("mleft")
 	if getPoints {
 		x, y := robotgo.GetMousePos()
-		fmt.Println("you clicked on", x, y)
+		fmt.Println("You clicked on", x, y)
 		return []int{x, y}
 	}
 	return []int{-1, -1}
